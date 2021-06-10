5b55c1226a667dff620b1a9d0fa01ea23a2462f0
Gregfanyan <grigorfanyan@gmail.com>
2021-06-10 14:56:49 +0200

    working on dynamic pages

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -2,7 +2,7 @@ import { Fragment } from "react";
 
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
       image="https://www.ebrdgreencities.com/assets/headers/bffc190ea7/yerevan-header.jpg"
@@ -12,5 +12,41 @@ function MeetupDetails() {
     />
   );
 }
+
+export const getStaticPaths = async () => {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupid: "m1",
+        },
+      },
+      {
+        params: {
+          meetupid: "m2",
+        },
+      },
+    ],
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const meetupId = context.params.meetupid;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://www.ebrdgreencities.com/assets/headers/bffc190ea7/yerevan-header.jpg",
+        id: meetupId,
+        title: "First meetup",
+        address: "Yerevan, Charentsi 7",
+        description: "The first meetup",
+      },
+    },
+    revalidate: 10,
+  };
+};
 
 export default MeetupDetails;
